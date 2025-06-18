f8de3a8d447b044748926a389088e19deffc92f8
Soo <109068985+thatgirls00@users.noreply.github.com>
2025-06-18 11:04:57 +0900

    feat: FND 표시 기능 추가 및 정렬 처리

---
+++ fnd_display.c
@@ -0,0 +1,10 @@
+void write_to_fnd(int* input, int len) {
+    int fd = open(FND_DEVICE, O_WRONLY);
+    if (fd < 0) return;
+    unsigned char fnd_data[4] = {0};
+    for (int i = 0; i < len && i < 4; i++)
+        if (input[i] != -1)
+            fnd_data[4 - len + i] = input[i];
+    write(fd, fnd_data, 4);
+    close(fd);
+}
